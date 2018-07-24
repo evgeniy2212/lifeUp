3c63edaf72918ef4d14da5b1bd3679807b03ca39
VladKabantsov <kabantsovlad@gmail.com>
2018-07-24 21:08:03 +0300

    change preview icon, change google analytic.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-MX2MVDD');</script>
+    <!-- End Google Tag Manager -->
+
+
     <title>Life Up</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -19,22 +29,15 @@
     {{--life-preview.jpg--}}
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-120617677-1"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
+    {{--<script async src="https://www.googletagmanager.com/gtag/js?id=UA-120617677-1"></script>--}}
+    {{--<script>--}}
+        {{--window.dataLayer = window.dataLayer || [];--}}
+        {{--function gtag(){dataLayer.push(arguments);}--}}
+        {{--gtag('js', new Date());--}}
 
-        gtag('config', 'UA-120617677-1');
-    </script>
+        {{--gtag('config', 'UA-120617677-1');--}}
+    {{--</script>--}}
 
-    <!-- Google Tag Manager -->
-    {{--<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':--}}
-                {{--new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],--}}
-            {{--j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=--}}
-            {{--'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);--}}
-        {{--})(window,document,'script','dataLayer','GTM-MX2MVDD');</script>--}}
-    <!-- End Google Tag Manager -->
     <script type="text/javascript" src="{{ asset('js/jquery-3.3.1.js') }}"></script>
     <script type="text/javascript" src="{{ asset('js/popper.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('js/jquery.circliful.min.js') }}"></script>
@@ -43,12 +46,13 @@
     <script type='text/javascript' src='{{ asset('js/general.js') }}'></script>
 </head>
 <body id="home-page">
-    <!-- Google Tag Manager (noscript) -->
-    {{--<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MX2MVDD"--}}
-                      {{--height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>--}}
-    <!-- End Google Tag Manager (noscript) -->
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MX2MVDD"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
-    <header>
+
+<header>
     <nav class="container">
         <ul class="soc-links">
             <li>
@@ -582,7 +586,7 @@
 								</span>
                         <span>Мои Интересы</span>
                     </div>
-                    <h4>александр сивчиков</h4>
+                    <h4>александр</h4>
                     <h5>Founder</h5>
                     <p>
                         William S. Burroughs said “It is not necessary to live. It is necessary to travel”. However, if you want the journey to be successful, it has to be planned, and it is a tough process. LifeUp helps not only to save your time, but also to organize the trip correctly, so you would travel with the “right” people.
